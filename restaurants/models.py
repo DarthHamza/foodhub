297bf643f401b13ac291c838d2a09fe553eefff4
DarthHamza <hamzamakia@gmail.com>
2017-11-05 16:37:22 +0300

    Task 4

--- restaurants/models.py
+++ restaurants/models.py
@@ -5,6 +5,10 @@ class Restaurant(models.Model):
 	description = models.TextField()
 	opening_time = models.TimeField()
 	closing_time = models.TimeField()
+	logo = models.ImageField(null=True)
 
 	def __str__(self):
 		return self.name
+
+	class Meta:
+		ordering = ['name',]
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Restaurant
+from .forms import RestaurantForm
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def restaurant_list(request):
 	object_list = Restaurant.objects.all()
+
+	paginator = Paginator(object_list, 5)
+	page = request.GET.get('page')
+	try:
+		objects = paginator.page(page)
+	except PageNotAnInteger:
+		objects = paginator.page(1)
+	except EmptyPage:
+		objects = paginator.page(paginator.num_pages)
+
 	context = {
-		"objects":object_list,
+		"objects":objects,
 	}
 	return render(request, 'restaurant_list.html', context)
 
@@ -14,3 +26,30 @@ def restaurant_detail(request, restaurant_id):
     "restaurant": instance
     }
     return render(request, 'restaurant_detail.html', context)
+
+def restaurant_create(request):
+    form = RestaurantForm(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        form.save()
+        return redirect("restaurant_list")
+    context = {
+    "form": form,
+    }
+    return render(request, 'restaurant_create.html', context)
+
+def restaurant_update(request, restaurant_id):
+    instance = Restaurant.objects.get(id=restaurant_id)
+    form = RestaurantForm(request.POST or None, request.FILES or None, instance = instance)
+    if form.is_valid():
+        form.save()
+        return redirect("restaurant_detail", restaurant_id=instance.id)
+    context = {
+    "form":form,
+    "instance": instance,
+    }
+    return render(request, 'restaurant_update.html', context)
+
+def restaurant_delete(request, restaurant_id):
+    instance = Restaurant.objects.get(id=restaurant_id)
+    instance.delete()
+    return redirect("restaurant_list")
